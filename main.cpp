db89f46fd1374bf0dedf4399883c9bd085ce2979
esbvaGames <esbva2002@gmail.com>
2020-08-23 16:51:37 -0400

    Colisiones + Bola

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 
 #include <iostream>
 
+enum CForce { forceLeft=1, forceRight, forceTop, forceDown };
+
 //. Para usar la libreria Standard
 using namespace std;
 //. Para usar la librería SFML Espacio de Nombres.-
 using namespace sf;
+
+int   Puntaje   = 0;
+bool  StartGame = false;
 
 Color colorBorde = Color( 74, 134, 232);
 Color colorBlock = Color(255,  90,  90);
@@ -21,7 +26,6 @@ Color colorVerde = Color(0, 255, 0);
 
 
 struct BLOCK {
-    RectangleShape rcBlock;
     //. Constructor que no hace nada, es una formalidad
     BLOCK() {};
 
@@ -38,6 +42,19 @@ struct BLOCK {
     void Display(RenderWindow *win){
         win->draw(rcBlock);
     }
+
+    void setActivo(bool state){
+       Activo = state;
+    }
+    bool isActivo() { return Activo; }
+
+    RectangleShape getRectangle(){
+       return rcBlock;
+    }
+
+protected:
+    bool Activo = true;
+    RectangleShape rcBlock;
 };
 
 //. Un contenedor de Bloques de 10 filas y 13 Columnas
@@ -131,12 +148,142 @@ RectangleShape Create_Rectangle(float px, float py, float width, float height, \
     return rcBase;
 }
 
+//. Control de la BOLA
+struct BOLA {
+
+   BOLA() {}
+
+   BOLA(float px, float py, float width, float height, Color myColor){
+      rcBola = Create_Rectangle(px, py, width, height, myColor);
+      rcBola.setOrigin((width / 2.0f) , (height / 2.0f));
+      this->px = px;
+      this->py = py;
+   }
+
+   void Update(){
+      if( !StartGame) { return; }
+      if( !Activa ){
+         Activa = true;
+         vx =  (rand() % 8) -4;
+         vy = -(rand() % 8);
+      }
+      px += vx;
+      py += vy;
+      rcBola.setPosition(px, py);
+   }
+
+   void reboteLeft()  { vx =  ((rand() % 8) -4) +2; }
+   void reboteRight() { vx = -((rand() % 8) -4) -2; }
+   void reboteTop()   { vy =  ((rand() % 8) -4) +2; vx = ((rand() % 8) -4) +2; }
+   void reboteDown()  { vy = -((rand() % 8) -4) -2; vx = ((rand() % 8) -4) -2; }
+
+
+   void Display(RenderWindow *win){
+      win->draw(rcBola);
+   }
+
+   RectangleShape getRectangle(){
+      return rcBola;
+   }
+
+//. Proteccion de datos, solo da acceso a clases heredadas,
+//. esta aca, solo porque el editor me muestra un color distinto de las funciones
+//. jajajaa.
+protected:
+   float Activa = false;
+   float px, py, vx, vy;   //. Velocidad en x,y
+   RectangleShape rcBola;
+
+};
+
+
+
+
+
+//. CONTROL DEL PLAYER
+struct PLAYER{
+   float px, py;
+   RectangleShape rcPlayer;
+
+   PLAYER() {}
+
+   PLAYER(float px, float py, float width, float height, Color myColor){
+      rcPlayer = Create_Rectangle(px, py, width, height, myColor);
+      rcPlayer.setOrigin(width / 2.0f, height / 2.0f);
+      this->px = px;
+      this->py = py;
+   }
+
+   void Update() {
+      if(Keyboard::isKeyPressed(Keyboard::Space)){
+         StartGame = true;
+      }
+
+      //. Solo funciona con una tecla presionada una vez;
+      if(Keyboard::isKeyPressed(Keyboard::A) && !Keyboard::isKeyPressed(Keyboard::D)){
+         px -= ((px > 76+1) ? 3 : 0);
+         rcPlayer.setPosition(px, py);
+         //cout << "Left: " << px << endl;
+      }
+
+      if(Keyboard::isKeyPressed(Keyboard::D) && !Keyboard::isKeyPressed(Keyboard::A)){
+         px += ((px < 480+1) ? 3 : 0);
+         rcPlayer.setPosition(px, py);
+         //cout << "Right: " << px << endl;
+      }
+
+   }
+
+   void Display(RenderWindow *win){
+      win->draw(rcPlayer);
+   }
+
+   RectangleShape getRectangle(){
+      return rcPlayer;
+   }
+
+};
+
+//. Calcular los Rectangulos de Colisiones
+bool isCollision(RectangleShape rcBola, RectangleShape rcBase, CForce *force){
+   Rect<float> myBola = Rect<float>(rcBola.getPosition().x, \
+                                    rcBola.getPosition().y, \
+                                    rcBola.getSize().x, \
+                                    rcBola.getSize().y);
+
+   //. Obtiene el punto Central del Rectangulo
+   Vector2f  ccBola = Vector2f(myBola.left + rcBola.getOrigin().x, \
+                               myBola.top  + rcBola.getOrigin().y);
+
+   //. Obtiene los rectangulos de los Bordes o Bloques
+   Rect<float> myBase = Rect<float>(rcBase.getPosition().x, \
+                                    rcBase.getPosition().y, \
+                                    rcBase.getSize().x,\
+                                    rcBase.getSize().y);
+
+   //. Obtiene el Centro de Rectangulo a comparar
+   Vector2f ccBase = Vector2f(myBase.left + (rcBase.getSize().x / 2.0f), \
+                              myBase.top  + (rcBase.getSize().y / 2.0f) );
+
+   if(myBola.intersects(myBase)){
+      if(ccBola.x > ccBase.x) { *force = CForce::forceLeft;  };
+      if(ccBola.x < ccBase.x) { *force = CForce::forceRight; };
+      if(ccBola.y > ccBase.y) { *force = CForce::forceTop;   };
+      if(ccBola.y < ccBase.y) { *force = CForce::forceDown;  };
+      return true;
+   }
+
+   return false;
+}
+
 
 
 
 int main()
 {
     sf::RenderWindow win(sf::VideoMode(800,480), "SFML Arkanoid by esbva");
+    //. Solo a 60 cuadros x Segundos
+    win.setFramerateLimit(60);
 
     //. Rectangulo  de Juego
     RectangleShape rcGame = Create_Rectangle(4,4, 550, 470, colorBorde);
@@ -150,7 +297,10 @@ int main()
     //. Colisionadores de las Compuertas
     RectangleShape rcDoor_L = Create_Rectangle(  14, 380, 32, 64, colorDoors);
     RectangleShape rcDoor_R = Create_Rectangle( 511, 380, 32, 64, colorDoors);
-    RectangleShape rcPlayer = Create_Rectangle( 250, 400, 64, 20, colorPlayer);
+    //RectangleShape rcPlayer = Create_Rectangle( 250, 400, 64, 20, colorPlayer);
+
+    BOLA *rcBola = new BOLA(250, 398, 16,16, Color( 90, 90,255));
+    PLAYER *rcPlayer = new PLAYER(250, 420, 64, 20, colorPlayer);
 
     Font font;
     if( !font.loadFromFile("./acme.ttf")){
@@ -218,17 +368,71 @@ int main()
 
       win.draw(rcDoor_L);
       win.draw(rcDoor_R);
-      win.draw(rcPlayer);
+
+
+      rcBola->Update();
+
+         CForce force;
+         if(isCollision(rcBola->getRectangle(), rcLeft, &force)){
+            cout << "Colision left: " << force << endl;
+            rcBola->reboteLeft();
+         }
+
+         if(isCollision(rcBola->getRectangle(), rcRight, &force)){
+            cout << "Colision right: " << force << endl;
+            rcBola->reboteRight();
+         }
+
+         if(isCollision(rcBola->getRectangle(), rcTop, &force)){
+            cout << "Colision top: " << force << endl;
+            rcBola->reboteTop();
+         }
+
+         if(isCollision(rcBola->getRectangle(), rcDown, &force)){
+            cout << "Colision Down - Bola perdida" << force << endl;
+            rcBola->reboteDown();
+         }
+
+
+
+         rcPlayer->Update();
+         rcPlayer->Display(&win);
+         if(isCollision(rcBola->getRectangle(), rcPlayer->getRectangle(), &force)){
+            if(force == CForce::forceLeft) { rcBola->reboteLeft(); }
+            if(force == CForce::forceRight) { rcBola->reboteRight(); }
+            rcBola->reboteDown();
+
+            Puntaje +=1;
+            cout << "Acierto del Player: " << Puntaje << endl;
+            char buffer[5]="";
+            sprintf(buffer, " Puntos: %-6i", Puntaje);
+            puntos.setString(string(buffer));
+         }
+
+      rcBola->Display(&win);
+
+
 
       //. Desplegar los bloques
+      //. Aprovechar el Mismo cyclo para Dibujar los Botones de los Niveles
+      int indice = 0;
       for(int index = 0; index < TOTAL; index++){
+        if(indice < LEVELS){
+          level[indice]->Display(&win);
+          indice++;
+        }
+        if( !block[index]->isActivo()) { continue; }
+        //. Colision con los Bloques
+        if(isCollision(rcBola->getRectangle(), block[index]->getRectangle(), &force)){
+           if(force == CForce::forceLeft ) { rcBola->reboteLeft(); }
+           if(force == CForce::forceRight) { rcBola->reboteRight(); }
+           if(force == CForce::forceTop)   { rcBola->reboteTop(); }
+           if(force == CForce::forceDown)  { rcBola->reboteDown(); }
+           block[index]->setActivo(false);
+           continue;
+        }
         block[index]->Display(&win);
       }
-
-      for(int index = 0; index < LEVELS; index++){
-        level[index]->Display(&win);
-      }
-
       win.display();
 
     }
